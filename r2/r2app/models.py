0001a217d916ea1f4a743d376862e7611c42ede9
PAUL LEE <pauly0418@gmail.com>
2022-05-31 22:15:53 -0700

    schema

--- r2/r2app/models.py
+++ r2/r2app/models.py
@@ -1,12 +1,27 @@
+from email.policy import default
 from pyexpat import model
 from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    title: models.CharField(max_length=255)
+    name: models.CharField(max_length=255)
     description: models.CharField
     content: models.CharField
     created_at: models.DateTimeField(auto_now_add=True)
     updated_at: models.DateTimeField(auto_now=True)
     deleted_at: models.DateTimeField
     deleted: models.BooleanField(default=False)
+
+class Subreddit(Thing):
+    subscriber_count: models.IntegerField(default=0)
+
+class Post(Thing):
+    subreddit: models.ForeignKey(Subreddit)
+    upvote_count: models.IntegerField(default=0)
+    downvote_count: models.IntegerField(default=0)
+
+class Comment(Thing):
+    post: models.ForeignKey(Post)
+    upvote_count: models.IntegerField(default=0)
+    downvote_count: models.IntegerField(default=0)
+    comment_tree: models.ManyToManyField('self', blank=True, symmetrical=False)
