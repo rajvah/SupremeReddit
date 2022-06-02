0554b9b542acb20b98210962e9154f46601b3502
PAUL LEE <pauly0418@gmail.com>
2022-06-01 21:40:26 -0700

    updated html

--- r2/r2app/models.py
+++ r2/r2app/models.py
@@ -2,6 +2,7 @@ from email.policy import default
 from pyexpat import model
 from django.db import models
 
+
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=255)
@@ -12,16 +13,20 @@ class Thing(models.Model):
     deleted_at = models.DateTimeField
     deleted = models.BooleanField(default=False)
 
-class Subreddit(Thing):
-    subscriber_count = models.IntegerField(default=0)
+
+#
+# class Subreddit(Thing):
+#     subscriber_count = models.IntegerField(default=0)
+
 
 class Post(Thing):
-    subreddit_field = models.ForeignKey(Subreddit, on_delete=models.SET_NULL, null=True)
+    # subreddit_field = models.ForeignKey(Subreddit, on_delete=models.SET_NULL, null=True)
     upvote_count = models.IntegerField(default=0)
     downvote_count = models.IntegerField(default=0)
 
-class Comment(Thing):
-    post_field = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
-    upvote_count = models.IntegerField(default=0)
-    downvote_count = models.IntegerField(default=0)
-    comment_tree = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+# class Comment(Thing):
+#     post_field = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+#     upvote_count = models.IntegerField(default=0)
+#     downvote_count = models.IntegerField(default=0)
+#     comment_tree = models.ManyToManyField('self', blank=True, symmetrical=False)
