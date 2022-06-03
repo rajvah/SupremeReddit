0a38d8002ce06209828dc0876243835405a0870d
ono-sendi <25675608+chase258@users.noreply.github.com>
2022-06-03 11:01:31 -0700

    pointers to cruds in wrapper

---
+++ r2/r2app/lib/db/clientHelper.py
@@ -0,0 +1,30 @@
+from cassandra.cluster import Cluster
+from cassandra.cqlengine.connection import setup
+
+
+def createClientCassandra():
+    cluster = Cluster()
+    setup(['127.0.0.1'], 'reddit', retry_connect=True)
+    client = cluster.connect()
+    setupTables(client)
+    # client.execute("create keyspace reddit with replication={ 'class': 'SimpleStrategy', 'replication_factor': 2};")
+    return client
+
+# TODO (Howard, Chase): is this taken care of by Paul's code?
+def createClientSQL():
+   pass
+
+def setupTables(client):
+    client.execute("use reddit")
+    client.execute(
+        "create table if not exists thing" +
+        "( id int," +
+        "name text," +
+        "description text," +
+        "content text," +
+        "create_at TIMESTAMP," +
+        "updated_at TIMESTAMP," +
+        "deleted_at TIMESTAMP," +
+        "deleted BOOLEAN," +
+        "PRIMARY KEY (id))"
+    )
